573568f16226431946604c2cfc568635f08c1d81
bulhakj <jakub.bulhak@gmail.com>
2025-01-07 15:51:56 +0100

    User logout controller

--- chatty_backend/src/features/auth/routes/authRoutes.ts
+++ chatty_backend/src/features/auth/routes/authRoutes.ts
@@ -1,6 +1,8 @@
 import { SignIn } from "@auth/controllers/signin";
+import { SignOut } from "@auth/controllers/signuout";
 import { SignUp } from "@auth/controllers/signup";
 import express, { Router } from "express";
+
 class AuthRoutes {
   private router: Router;
 
@@ -11,6 +13,12 @@ class AuthRoutes {
   public routes(): Router {
     this.router.post("/signup", SignUp.prototype.create);
     this.router.post("/signin", SignIn.prototype.read);
+
+    return this.router;
+  }
+
+  public signoutRoute(): Router {
+    this.router.get("/signout", SignOut.prototype.update);
 
     return this.router;
   }
